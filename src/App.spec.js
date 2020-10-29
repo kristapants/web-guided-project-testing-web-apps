be622b08aa0fb5c1238412de265ee0e45b51e156
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-10-29 12:48:04 -0400

    lecture work-in-progress

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,6 +1,20 @@
 import React from "react";
 import { render } from '@testing-library/react';
 
-test("This is a simple test", ()=>{
+
+
+it("This is a simple test 1", ()=>{
+
+});
+
+test("This is a simple test 2", ()=>{
+    
+});
+
+test("This is a simple test 3", ()=>{
+    
+});
+
+test("This is a simple test 4", ()=>{
     
 });
