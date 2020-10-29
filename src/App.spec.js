b70cd496a7077c4526c3b72d0f39e7101d29231c
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-10-29 12:50:29 -0400

    lecture work-in-progress

--- src/App.spec.js
+++ src/App.spec.js
@@ -4,25 +4,7 @@ import { render } from '@testing-library/react';
 
 describe("Spec.s js tests", ()=>{
     test("This is a simple test 1", ()=>{
-        throw new Error("This is an error");
-    });
-    
-    test("This is a simple test 2", ()=>{
         
-    });
-    
-    test("This is a simple test 3", ()=>{
-        
-    });
-    
-    test("This is a simple test 4", ()=>{
-        
-    });
-});
-
-describe("test.js tests", ()=>{
-    test("This is a simple test 1", ()=>{
-
     });
     
     test("This is a simple test 2", ()=>{
